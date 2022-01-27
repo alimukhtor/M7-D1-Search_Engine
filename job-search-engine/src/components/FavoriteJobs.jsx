fc40162d3bf1ced51317051fa35d1f9e6184de66
alimukhtor <james1999007@gmail.com>
2022-01-27 12:47:47 +0100

    updating

--- job-search-engine/src/components/FavoriteJobs.jsx
+++ job-search-engine/src/components/FavoriteJobs.jsx
@@ -1,4 +1,4 @@
-import { ListGroup, Button } from 'react-bootstrap'
+import { ListGroup, Button, Row, Col } from 'react-bootstrap'
 import { RiDeleteBin6Line } from "react-icons/ri";
 import {connect} from 'react-redux'
 
@@ -23,10 +23,14 @@ const FavoriteJobs =({favorites, removeFromFavorite})=> {
         <ListGroup>
             {
                 favorites && favorites.map((fav, i)=> (
-                    <>
-                    <ListGroup.Item key={i}>{fav.title}</ListGroup.Item>
-                    <Button variant="danger" onClick={() => {removeFromFavorite(i)}}><RiDeleteBin6Line /></Button>
-                    </>
+                    <Row>
+                        <Col>
+                            <ListGroup.Item key={i}>{fav.title}</ListGroup.Item>
+                        </Col>
+                        <Col>
+                            <Button variant="danger" onClick={() => {removeFromFavorite(i)}}><RiDeleteBin6Line  className="ml-auto" /></Button>
+                        </Col>
+                    </Row>
                 ))
             }
         </ListGroup>
