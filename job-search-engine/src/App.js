b256f0d6e32742c13947bd61e2e9dc5c8de2c85f
alimukhtor <james1999007@gmail.com>
2022-01-31 13:17:50 +0100

    updated

--- job-search-engine/src/App.js
+++ job-search-engine/src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <Container>
     <div className="App">
+      <h1>Hello!</h1>
        <BrowserRouter>
      	 <Routes>
          <Route path="/" element={<Home />} />
